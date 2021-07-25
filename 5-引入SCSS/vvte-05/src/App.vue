61e30f394a8d02fb20a4d239d87cb8815bf7b4c1
David <zw_work@aliyun.com>
2021-07-25 19:36:01 +0800

    5-引入SCSS

---
+++ 5-引入SCSS/vvte-05/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Study />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Study from '@/components/Study.vue'
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Study,
+  },
+});
+</script>
