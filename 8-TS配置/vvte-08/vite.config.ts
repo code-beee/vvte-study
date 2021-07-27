f83dbf1cdd4330701eefb75c6147ddbf7fcd30c0
David <zw_work@aliyun.com>
2021-07-27 22:26:35 +0800

    8-TS配置

---
+++ 8-TS配置/vvte-08/vite.config.ts
@@ -0,0 +1,29 @@
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
+import { loadEnv } from 'vite';
+
+export default ({ command, mode }) => {
+	// ↓加载环境变量
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+  	// ↓插件配置
+    plugins: [vue()],
+    // ↓解析配置
+    resolve: {
+      // ↓路径别名
+      alias: {
+        // ↓举例：@/abc => src/abc
+        '@': resolve(__dirname, "src")
+      }
+    },
+    // ↓服务端配置
+    server: {
+      // ↓读取环境变量中的HOST、PORT、OPEN
+      host: env['VITE_HOST'],
+      port: env['VITE_PORT'],
+      open: env['VITE_OPEN'],
+      strictPort: true
+    }
+  }
+}
