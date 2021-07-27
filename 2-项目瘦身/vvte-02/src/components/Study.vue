e1b7e667cb33a827f63381ab9cfc3f869b27a129
David <zw_work@aliyun.com>
2021-07-27 10:48:33 +0800

    2-项目瘦身

---
+++ 2-项目瘦身/vvte-02/src/components/Study.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Hello World!</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Study",
+  setup() {
+    return {};
+  },
+});
+</script>
