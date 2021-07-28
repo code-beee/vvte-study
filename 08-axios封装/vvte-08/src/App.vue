a6c6f3dc5bf78a1251f51b31f51cce7a9cda98e1
David <zw_work@aliyun.com>
2021-07-29 00:03:35 +0800

    08-axios封装

--- 08-axios封装/vvte-08/src/App.vue
+++ 08-axios封装/vvte-08/src/App.vue
@@ -4,9 +4,9 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
-})
+  name: "App",
+});
 </script>
---
+++ 08-axios封装/vvte-08/src/components/demos/Axios.vue
@@ -0,0 +1,48 @@
+<template>
+  <!-- ↓HTTP请求demo -->
+  <div>
+    <h2>HTTP请求</h2>
+    <button @click="httpGet">get请求</button>
+    <button @click="httpPost">post请求</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { axiosInstance } from "@/utils/http/index";
+
+export default defineComponent({
+  name: "Axios",
+  setup() {
+    // ↓发送get请求
+    const httpGet = () => {
+      axiosInstance
+        .get("https://jsonplaceholder.typicode.com/posts/1")
+        .then((response: any) => {
+          console.log(response);
+        })
+        .catch((error: any) => {
+          alert(error);
+        });
+    };
+    // ↓发送post请求
+    const httpPost = () => {
+      axiosInstance
+        .post("https://jsonplaceholder.typicode.com/posts", {
+          keyword: "code",
+        })
+        .then((response: any) => {
+          console.log(response);
+        })
+        .catch((error: any) => {
+          alert(error);
+        });
+    };
+
+    return {
+      httpGet,
+      httpPost,
+    };
+  },
+});
+</script>
