1134c2af19825746a9b275c932bfdde6b2a41bb8
David <zw_work@aliyun.com>
2021-08-03 02:14:32 +0800

    10-Prettier配置

---
+++ 10-Prettier配置/vvte-10/src/components/demos/Env.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- ↓环境变量demo -->
+  <div>
+    <h2>环境变量</h2>
+    mode: {{ mode }} <br />
+    host: {{ host }} <br />
+    port: {{ port }} <br />
+    baseUrl: {{ baseUrl }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Env",
+  setup() {
+    // ↓读取内建环境变量
+    const mode = import.meta.env.MODE;
+    // ↓读取自定义环境变量
+    const host = import.meta.env.VITE_HOST;
+    const port = import.meta.env.VITE_PORT;
+    const baseUrl = import.meta.env.VITE_BASE_URL;
+    // ↓返回变量，使支持template获取
+    return {
+      mode,
+      host,
+      port,
+      baseUrl,
+    };
+  },
+});
+</script>
---
+++ 10-Prettier配置/vvte-10/src/components/layout/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2 class="red">固定区域</h2>
+  <!-- ↓layout路由视图 -->
+  <router-view></router-view>
+</template>
+
+<script>
+import '@/styles/index.scss'
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Layout",
+});
+</script>
